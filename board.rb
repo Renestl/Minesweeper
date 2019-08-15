a81dbc19b34fe635d2a33cdbc5f384014aaf83d5
Jennifer Currie <renestl@gmail.com>
2019-08-15 02:52:56 -0500

    Initialize board

--- board.rb
+++ board.rb
@@ -3,12 +3,22 @@ require_relative 'tile'
 class Board
 	attr_reader :grid	
 
-	def initialize(size = 9)
-		@size = size * size
-		@grid = Array.new(size) {Array.new(size)}
+	def initialize(size = 4)
+		@size = size
 
-		populate_bombs
+		populate_grid
 	end
+
+	def populate_grid
+		@grid = Array.new(@size) do |row|
+			Array.new(@size) do |col|
+				Tile.new(self, [row, col])
+			end
+		end
+
+		# populate_bombs
+	end
+
 
 	def populate_bombs
 		bombs = @size * 0.20
@@ -28,13 +38,11 @@ class Board
 
 	def [](board_position)
 		row, col = board_position
-
 		@grid[row][col]
 	end
 
 	def []=(position, value)
 		row, col = position
-
 		@grid[row][col] = value
 	end
 	
